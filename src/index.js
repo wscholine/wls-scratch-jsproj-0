e59a6c9150784a56fc941a3ec4fd4d444908f9c1
Wade L. Scholine <wscholine@dssinc.com>
2023-01-27 12:01:22 -0500

    cleaned up

--- src/index.js
+++ src/index.js
@@ -1,35 +1,37 @@
 import * as fs from 'node:fs';
 import { open } from 'node:fs/promises';
 
-console.log('Hello world')
-
 let stuff = [];
 let nextUrl = 'https://api.zynx.com/t/zynx.com/connect/1.0.0/PlanDefinition/';
 let fileDescriptor = null;
 
+
+function findNextLink(json)
+{
+    return json.link.filter(x => x && x.relation && x.relation === 'next' && x.url)
+}
+
+
+function planDefPredicate(resource) {
+    return resource.type.coding.filter(x => x.code === 'clinical-protocol').length > 0;
+}
+
 async function getStuff(url) {
-    await fetch(url, {
+    return await fetch(url, {
         headers: {
             Accept: 'application/json',
             Authorization: 'Bearer 57b438ee-91bc-366f-92b4-7e581e89aea6',
         }
     }).
-    then((response) => {
-        
-        return response.json();
-    }).
+    then((response) => response.json()).
     then(json => {
-        nextUrl = json.link[1] ? json.link[1].url : '';
-        console.log(`in fetch: nextUrl: ${nextUrl}`)
+        const nextLink = findNextLink(json);
+        nextUrl = nextLink[0].url
         return json;
     }).
-    then((data) => data.entry.filter(x => x.resource.type.coding[0].code === 'clinical-protocol')).
+    then((data) => data.entry.filter(x => planDefPredicate(x.resource))).
     then(entries => {
-        console.log(entries)
-        // fileDescriptor.appendFile(JSON.stringify(entries[0]))
-        // fileDescriptor.appendFile(',')
         stuff.push(entries)
-        console.log(`entries.length: ${entries.length}`)
         return entries.length;
     });
 }
@@ -37,13 +39,12 @@ async function getStuff(url) {
 async function doMyThing(url) {
     let plansFound = 0
     let nCopies = 0
-//    fileDescriptor = await open('./zynx-data-0', 'a+')
     do {        
         plansFound = await getStuff(url);
         console.log(`plans found: ${plansFound}, stuff.length: ${stuff.length}`)
         console.log(`nextUrl: ${nextUrl}`)
-    } while (nextUrl != '' && ++nCopies < 51)
-    console.log(`stuff.length: ${stuff.length}`)
+    } while (nextUrl != '' && ++nCopies < 4)
+    console.log(`ending stuff.length: ${stuff.length}`)
     const foo = {
         planDefs : stuff
     }
